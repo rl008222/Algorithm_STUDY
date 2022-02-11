b85700546aad0a62bc3c6879c8a4d72b24af8d03
LeeKiJong <kijong0822@gmail.com>
2022-02-11 16:10:17 +0900

    0211-12927

---
+++ baekjoon/B_12927.java
@@ -0,0 +1,27 @@
+package a0211;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//저번 IM 문제
+public class B_12927 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char []c = br.readLine().toCharArray();
+		for(int i=0;i<c.length;i++) {
+			boolean check = true;
+			for(int j=0;j<c.length;j++) {
+				
+			}
+			for(int j=0;j<c.length;j++) {
+				if(j=='N') check = false;
+			}
+		}
+	}
+}
