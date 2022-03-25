f610a0f98f65bbc1ca2e884dcd523efde949dc25
LeeKiJong <kijong0822@gmail.com>
2022-03-25 21:15:51 +0900

    0325-멀쩡한사각형

---
+++ Programmers/P_멀쩡한사각형.java
@@ -0,0 +1,25 @@
+package a0325;
+
+public class P_멀쩡한사각형 {
+	public static void main(String[] args) {
+		System.out.println(solution(8, 12) + "");
+	}
+	public static long solution(int w, int h) {
+		long answer = (long)w*h - (w+h - gcd(w, h));
+        return answer;
+    }
+	public static int gcd(int w, int h) {
+		int a=w;
+		int b=h;
+		if(a<b) {
+			b=w;
+			a=h;
+		}
+		while(b>0) {
+			int r=a%b;
+			a=b;
+			b=r;
+		}
+		return a;
+	}
+}
