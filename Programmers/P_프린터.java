9b0d09fac4ded2dbb5fda9235a4e7c148a00887c
LeeKiJong <kijong0822@gmail.com>
2022-04-20 21:04:19 +0900

    0420-프린터

--- Programmers/P_프린터.java
+++ Programmers/P_프린터.java
@@ -1,18 +1,29 @@
-package a0322;
+package a0420;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class P_프린터 {
 	public static void main(String[] args) {
-		int[] a = { 1, 1, 9, 1, 1, 1};
-
-		System.out.println(solution(a, 0));
+		System.out.println(solution(new int[] {2, 1, 3, 2 }, 2) + "");
 	}
-	static class loc{
-		int x; int y;
+	public static class loc implements Comparable<loc>{
+		int x,y;
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
 
 		public loc(int x, int y) {
 			super();
@@ -20,36 +31,49 @@ public class P_프린터 {
 			this.y = y;
 		}
 
-		public int getX() {
-			return x;
+		@Override
+		public int compareTo(loc o) {
+			return o.y-y;
 		}
-
-		public int getY() {
-			return y;
-		}
+		
 	}
 	public static int solution(int[] priorities, int location) {
 		int answer = 0;
-		Queue<loc> q = new LinkedList<>();
-		ArrayList<Integer> list = new ArrayList<>();
+		PriorityQueue<loc> q = new PriorityQueue<>();
 		for(int i=0;i<priorities.length;i++) {
-			q.add(new loc(priorities[i], i));
-			list.add(priorities[i]);
-		}
-		Collections.sort(list, Collections.reverseOrder());
+	    	q.add(new loc(i, priorities[i]));
+	    }
 		int cnt=0;
-		for(int i=0;;) {
-			if(q.peek().getX()==list.get(i)) {
-				cnt++;
-				i++;
-				if(q.poll().getY()==location) {
-					answer=cnt;
-					break;
-				}
-			}else {
-				q.add(q.poll());
+		while(!q.isEmpty()) {
+			cnt++;
+			if(q.poll().x==location) {
+				answer=cnt;
+				break;
 			}
 		}
-		return answer;
+		/*
+	    Queue<loc> q = new LinkedList<>();
+	    List<Integer> list = new LinkedList<>();
+	    for(int i=0;i<priorities.length;i++) {
+	    	q.add(new loc(i, priorities[i]));
+	    	list.add(priorities[i]);
+	    }
+	    Collections.sort(list, Collections.reverseOrder());
+	    int cnt=0;
+	    for(int i=0;;) {
+	    	if(q.peek().getY()==list.get(i)) {
+	    		cnt++;
+	    		i++;
+	    		if(q.poll().getX()==location) {
+	    			answer=cnt;
+	    			break;
+	    		}
+	    	}else {
+	    		q.add(q.poll());
+	    	}
+	    }
+	    */
+		
+	    return answer;
 	}
 }
